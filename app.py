deb79a4c5866c18392892c7358b61f2b32cde1ba
s-exe <32960849+s-exe@users.noreply.github.com>
2023-09-24 14:18:19 +1000

    show plots trial

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -13,6 +17,27 @@ def predict(input_data):
     # Replace this with your model prediction logic
     # return {'prediction': 'Yes', 'confidence': 0.75}
     return prediction
+
+def create_plot():
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+
+    plt.plot(x, y)
+    plt.title('Sine Wave')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+def plot_to_img():
+    create_plot()
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+
+    img_b64 = base64.b64encode(img.getvalue()).decode()
+
+    return img_b64
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -30,3 +55,13 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/plot')
+def plot():
+
+    img_b64 = plot_to_img()
+
+    html = f'<img src="data:image/png;base64,{img_b64}" class="blog-image">'
+
+    return render_template_string(html)
