e52027f5a973e6f006e3e984f1960c8d983321f1
s-exe <32960849+s-exe@users.noreply.github.com>
2023-09-24 14:41:50 +1000

    added render_template_string function in flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, render_template_string
 import pickle
 import matplotlib.pyplot as plt
 import numpy as np
